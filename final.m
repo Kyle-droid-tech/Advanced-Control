4880e67314ab8f2081aea008bc3e481fabe54967
Kyle-droid-tech <57788349+Kyle-droid-tech@users.noreply.github.com>
2020-05-26 21:34:08 +0900

    Update final.m

--- final.m
+++ final.m
@@ -11,36 +11,43 @@ eig(A);
 sys=ss(A,B,C,D);
 
 x0=[1 0];
-%initial(sys,x0);
+f0=figure;
+initial(sys,x0);
 
-
+f1=figure;
 p1=[-1,-3];
 K1=place(A,B,p1);
 A1=A-B*K1;
 eig(A1);
-%sys1=ss(A1,[],C,[]);
-%initial(sys1,x0);
+sys1=ss(A1,[],C,[]);
+initial(sys1,x0);
+
 %[b,a]=ss2tf(A1,B,C,D);
 %Path=[b,a];
-%sys2=tf([0 0 1],[1 4 3])
-%C2=[1 0;0 1;-K1];
-%sys2=ss(A1,[],C2,[]);
-%initial(sys2,x0);
 
-%p2=[-2,-3];
-%K2=place(A,B,p2);
-%A2=A-B*K2;
-%eig(A2);
-%sys2=ss(A2,[],C,[]);
-%initial(sys2,x0);
+f2=figure;
+sys2=tf([0 0 1],[1 4 3])
+C2=[1 0;0 1;-K1];
+sys2=ss(A1,[],C2,[]);
+initial(sys2,x0);
 
-%p3=[-1,-4];
-%K3=place(A,B,p3);
-%A3=A-B*K3;
-%eig(A3);
-%sys3=ss(A3,[],C,[]);
-%initial(sys3,x0);
+f02=figure;
+p2=[-2,-3];
+K2=place(A,B,p2);
+A2=A-B*K2;
+eig(A2);
+sys2=ss(A2,[],C,[]);
+initial(sys2,x0);
 
+f03=figure;
+p3=[-1,-4];
+K3=place(A,B,p3);
+A3=A-B*K3;
+eig(A3);
+sys3=ss(A3,[],C,[]);
+initial(sys3,x0);
+
+f04=figure;
 Q4=[10 0;0 0];
 R4=[1];
 [K4,S,e]=lqr(A,B,Q4,R4);
@@ -50,6 +57,7 @@ C4=[1 0;1 0;-K4];
 sys4=ss(A4,[],C4,[]);
 initial(sys4,x0);
 
+f05=figure;
 Q5=[1 0;0 0];
 R5=[1];
 [K5,S,e]=lqr(A,B,Q5,R5);
@@ -59,6 +67,7 @@ C5=[1 0;1 0;-K5];
 sys5=ss(A5,[],C5,[]);
 initial(sys5,x0);
 
+f06=figure;
 Q6=[10 0;10 0];
 R6=[1];
 [K6,S,e]=lqr(A,B,Q6,R6);
@@ -68,6 +77,7 @@ C6=[1 0;1 0;-K6];
 sys6=ss(A6,[],C6,[]);
 initial(sys6,x0);
 
+f07=figure;
 Q7=[100 0;0 10];
 R7=[1];
 [K7,S,e]=lqr(A,B,Q7,R7);
